784d785109d9a243b2d3eba65dca38a49a01f413
Anapi <anapi.vallega@student.passerellesnumeriques.org>
2023-01-24 21:43:07 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
 import { LogInComponent } from './log-in/log-in.component';
 
 
@@ -20,7 +15,6 @@ import { LogInComponent } from './log-in/log-in.component';
 @NgModule({
   declarations: [
     AppComponent,
-
     HeaderComponent,
     SidenavComponent,
     HomeComponent,
@@ -30,7 +24,7 @@ import { LogInComponent } from './log-in/log-in.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
+    NgModule,
     MatAutocompleteModule,
     BrowserAnimationsModule
   ],

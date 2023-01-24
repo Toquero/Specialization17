75ef4516c2e49b1056782ff0e89fccfabbbb3d4c
Anapi <anapi.vallega@student.passerellesnumeriques.org>
2023-01-24 21:12:33 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { LogInComponent } from './log-in/log-in.component';
-=======
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
->>>>>>> a669b86c89a321fdb31e344ff1895cec30603b21
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    
   ],
   imports: [
     BrowserModule,

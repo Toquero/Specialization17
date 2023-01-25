6ec4601120d8e32691245fa3a39ab54be7fdb65b
Toquero <toquerochristy08@gmail.com>
2023-01-25 23:05:19 +0800

    seven

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch:'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'dashboard', component: DashboardComponent},
+  {path: 'home', component: HomeComponent },
+  {path: '', component: HomeComponent},
+  {path: 'ourteam', component: OurteamComponent},
+  {path: 'contact', component: ContactComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-<<<<<<< HEAD
+import { FormsModule } from '@angular/forms';
 
-=======
->>>>>>> 630c54debff879017e34cb0b851814198fd87bf6
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
 import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-<<<<<<< HEAD
+
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { LogInComponent } from './log-in/log-in.component';
-=======
-import { LogComponent } from './log/log.component';
-
->>>>>>> 630c54debff879017e34cb0b851814198fd87bf6
-
-
+import {MatListModule} from '@angular/material/list';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SidenavComponent,
     HomeComponent,
-    DashboardComponent,
-    LogComponent,
+    OurteamComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgModule,
     MatAutocompleteModule,
     BrowserAnimationsModule,
     MatIconModule,
     MatDividerModule,
     MatMenuModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
